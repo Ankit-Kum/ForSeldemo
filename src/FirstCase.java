df716546be121065e425ff0f163ac7ef52a5571c
Ankit-Kum <61354499+Ankit-Kum@users.noreply.github.com>
2022-08-13 14:02:35 +0530

    Update FirstCase.java

--- src/FirstCase.java
+++ src/FirstCase.java
@@ -59,7 +59,7 @@ public class FirstCase {
 	
 	
 	
-	public static void (String[] args) throws InterruptedException {
+	public static void main  (String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 	
 		FirstCase ob = new FirstCase();
